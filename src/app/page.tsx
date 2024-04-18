457c0f94ca50d76a9173c9e482ad669b31b4a7f1
Shardul9577 <shardul1210@gmail.com>
2024-04-18 15:37:03 +0530

    Your detailed commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,3 @@
-// "use client";
-// import Image from "next/image";
-// import Link from 'next/link';
-// import { StickyScrollRevealDemo } from "./components/Product";
-// import  DrawerAppBar  from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import './components/css/Mainpage.css'
-// import { HeroHighlightDemo } from "./components/globeDemo";
-// import { LayoutGridDemo } from "./components/LayoutGridDemo";
-// import { TracingBeamDemo } from "./components/TracingBeamDemo";
-// import {Head} from 'next/document'
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <Head>
-//           <link rel="icon" href="/favicon.ico" />
-//           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-//           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-//           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-//       </Head>
-//       <DrawerAppBar/>
-
-//             <div style={{width:"100vw",height:"100vh"}}>
-//                 <HeroHighlightDemo/>
-//             </div>
-
-//             <h1 className="general-heading">Real Cases</h1>          
-
-//             <LayoutGridDemo/>
-
-//             <h1 className="general-heading">Inspiration & Abstract</h1>
-
-//             {/* <div className="cards-alignment">
-//             <Link legacyBehavior href="/about">
-
-//               <div className="pin-card">
-//                 <AnimatedPinDemo />
-//               </div>
-
-//             </Link>
-//             <Link legacyBehavior href="/about">
-              
-//               <div className="pin-card">
-//                 <AnimatedPinSDemo />
-//               </div>
-
-//             </Link>
-//             </div> */}
-
-//             <TracingBeamDemo/>
-
-
-//             <div>
-//               <h1 className="general-heading">Product</h1>
-//               <StickyScrollRevealDemo />
-//             </div>
-
-//             <Footer />
-//     </main>
-//   );
-// }
-
-// ______________________________________________________________________________
-
 import Image from "next/image";
 import Link from 'next/link';
 import { StickyScrollRevealDemo } from "./components/Product";
@@ -71,7 +6,7 @@ import './components/css/Mainpage.css';
 import { HeroHighlightDemo } from "./components/globeDemo";
 import { LayoutGridDemo } from "./components/LayoutGridDemo";
 import { TracingBeamDemo } from "./components/TracingBeamDemo";
-import Head from 'next/head'; // Correct import for the head
+import Head from 'next/head';
 import Navbar from "./components/Navbar";
 
 export default function Home() {
@@ -82,7 +17,7 @@ export default function Home() {
           <link rel="icon" type="image/png" sizes="32x32" href="/Images/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/Images/favicon-16x16.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/Images/apple-touch-icon.png" />
-          <title>Home - Your Site Name</title> // Add a title or other meta tags here
+          <title>Home - Your Site Name</title> 
       </Head>
 
       <Navbar/>
@@ -96,20 +31,6 @@ export default function Home() {
       <LayoutGridDemo/>
 
       <h1 className="general-heading">Inspiration & Abstract</h1>
-
-      {/* Commented section remains the same. If needed uncomment and use */}
-      {/* <div className="cards-alignment">
-        <Link legacyBehavior href="/about">
-          <div className="pin-card">
-            <AnimatedPinDemo />
-          </div>
-        </Link>
-        <Link legacyBehavior href="/about">
-          <div className="pin-card">
-            <AnimatedPinSDemo />
-          </div>
-        </Link>
-      </div> */}
 
       <TracingBeamDemo/>
 
