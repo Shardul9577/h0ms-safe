a74abcac8ec42b5bf21b962b5c64ff7299347bba
Shardul9577 <shardul1210@gmail.com>
2024-04-18 15:27:46 +0530

    Your detailed commit message

--- src/app/components/LayoutGridDemo.tsx
+++ src/app/components/LayoutGridDemo.tsx
@@ -141,14 +141,13 @@
 //   },
 // ];
 
-
 "use client";
 import React from "react";
 import { LayoutGrid } from "../ui/layout-grid";
 
 export function LayoutGridDemo() {
   return (
-    <div className="h-screen py-20 w-full">
+    <div className='h-screen py-20 w-full'>
       <LayoutGrid cards={cards} />
     </div>
   );
@@ -157,9 +156,14 @@ export function LayoutGridDemo() {
 const SkeletonOne = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Accident Capital</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        As of my last update, Uttar Pradesh was often referred to as the "accident capital of India." This state has reported a high number of road accidents annually, largely due to its large population and extensive road network. Factors contributing to its high accident rate include poor road conditions, high vehicle density, and sometimes lax enforcement of traffic laws.
+      <p className='font-bold text-4xl text-white'>Accident Capital</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        As of my last update, Uttar Pradesh was often referred to as the
+        &quot;accident capital of India&quot;. This state has reported a high number of
+        road accidents annually, largely due to its large population and
+        extensive road network. Factors contributing to its high accident rate
+        include poor road conditions, high vehicle density, and sometimes lax
+        enforcement of traffic laws.
       </p>
     </div>
   );
@@ -168,9 +172,16 @@ const SkeletonOne = () => {
 const SkeletonTwo = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">The Figures</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        In India, fatal road accidents are commonly caused by over-speeding, which leads to severe crashes due to reduced reaction times. Drunk driving and wrong-side driving are also significant factors, both resulting in impaired judgment and dangerous collisions. Using mobile phones while driving and jumping red lights contribute to distracted driving and risky intersection incidents. Adverse weather conditions, vehicle defects such as brake failures, and poor road maintenance like unmarked hazards increase accident risks.
+      <p className='font-bold text-4xl text-white'>The Figures</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        In India, fatal road accidents are commonly caused by over-speeding,
+        which leads to severe crashes due to reduced reaction times. Drunk
+        driving and wrong-side driving are also significant factors, both
+        resulting in impaired judgment and dangerous collisions. Using mobile
+        phones while driving and jumping red lights contribute to distracted
+        driving and risky intersection incidents. Adverse weather conditions,
+        vehicle defects such as brake failures, and poor road maintenance like
+        unmarked hazards increase accident risks.
       </p>
     </div>
   );
@@ -179,9 +190,16 @@ const SkeletonTwo = () => {
 const SkeletonThree = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Red Zones</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Typically, Uttar Pradesh, including its city Kanpur, reports a high number of fatalities due to road accidents, often leading among states in India. Detailed and specific statistics, such as the total number of deaths due to accidents in Uttar Pradesh and particularly in Kanpur for a given year, would be available in the annual reports published by the Ministry of Road Transport and Highways (MoRTH) or local governmental sources. These reports detail fatalities and are critical for understanding regional trends in road safety.
+      <p className='font-bold text-4xl text-white'>Red Zones</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        Typically, Uttar Pradesh, including its city Kanpur, reports a high
+        number of fatalities due to road accidents, often leading among states
+        in India. Detailed and specific statistics, such as the total number of
+        deaths due to accidents in Uttar Pradesh and particularly in Kanpur for
+        a given year, would be available in the annual reports published by the
+        Ministry of Road Transport and Highways (MoRTH) or local governmental
+        sources. These reports detail fatalities and are critical for
+        understanding regional trends in road safety.
       </p>
     </div>
   );
@@ -190,9 +208,15 @@ const SkeletonThree = () => {
 const SkeletonFour = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Sure Signs</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        In highly congested cities like Mumbai and Delhi, common signs of accidents include high-speed crashes during off-peak hours and collisions at congested intersections during peak times. In cities with poor road infrastructure like Patna, accidents frequently occur due to inadequate road maintenance and poorly marked hazards. Seasonal weather conditions, like fog in northern cities such as Chandigarh, also significantly contribute to accident rates by reducing visibility.
+      <p className='font-bold text-4xl text-white'>Sure Signs</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        In highly congested cities like Mumbai and Delhi, common signs of
+        accidents include high-speed crashes during off-peak hours and
+        collisions at congested intersections during peak times. In cities with
+        poor road infrastructure like Patna, accidents frequently occur due to
+        inadequate road maintenance and poorly marked hazards. Seasonal weather
+        conditions, like fog in northern cities such as Chandigarh, also
+        significantly contribute to accident rates by reducing visibility.
       </p>
     </div>
   );
@@ -201,9 +225,14 @@ const SkeletonFour = () => {
 const SkeletonFive = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">The Day of Accident</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Historically, research and traffic studies have indicated that weekends, especially Saturdays, tend to see a higher number of road accidents in cities like Mumbai. This trend is generally attributed to increased travel during the weekend, with more vehicles on the road and a higher likelihood of drivers under the influence of alcohol due to social activities.
+      <p className='font-bold text-4xl text-white'>The Day of Accident</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        Historically, research and traffic studies have indicated that weekends,
+        especially Saturdays, tend to see a higher number of road accidents in
+        cities like Mumbai. This trend is generally attributed to increased
+        travel during the weekend, with more vehicles on the road and a higher
+        likelihood of drivers under the influence of alcohol due to social
+        activities.
       </p>
     </div>
   );
@@ -212,9 +241,15 @@ const SkeletonFive = () => {
 const SkeletonSix = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Multiple Reasons</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Road accidents are commonly caused by a combination of human error, environmental factors, and vehicle malfunctions. Over-speeding, drunk driving, distracted driving (such as using mobile phones), and non-compliance with traffic rules are predominant human-related causes. Environmental factors include poor road conditions, adverse weather, and inadequate street lighting, while vehicle-related issues often involve failures like brake defects or tire blowouts.
+      <p className='font-bold text-4xl text-white'>Multiple Reasons</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        Road accidents are commonly caused by a combination of human error,
+        environmental factors, and vehicle malfunctions. Over-speeding, drunk
+        driving, distracted driving (such as using mobile phones), and
+        non-compliance with traffic rules are predominant human-related causes.
+        Environmental factors include poor road conditions, adverse weather, and
+        inadequate street lighting, while vehicle-related issues often involve
+        failures like brake defects or tire blowouts.
       </p>
     </div>
   );
@@ -223,9 +258,14 @@ const SkeletonSix = () => {
 const SkeletonSeven = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Delhi</p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        In Delhi, road accidents are primarily attributed to over-speeding, reckless driving, and non-adherence to traffic rules. The high volume of traffic and poor compliance with road safety norms further compound these issues. Additionally, driving under the influence of alcohol and the presence of unmarked construction zones are significant contributors to the city's road safety challenges.
+      <p className='font-bold text-4xl text-white'>Delhi</p>
+      <p className='font-normal text-base my-4 max-w-lg text-neutral-200'>
+        In Delhi, road accidents are primarily attributed to over-speeding,
+        reckless driving, and non-adherence to traffic rules. The high volume of
+        traffic and poor compliance with road safety norms further compound
+        these issues. Additionally, driving under the influence of alcohol and
+        the presence of unmarked construction zones are significant contributors
+        to the city road safety challenges.
       </p>
     </div>
   );
@@ -282,4 +322,3 @@ const cards = [
       "https://cdn.cseindia.org/userfiles/Tribune-Delhi-24June-1-Road-Safety-CSE.jpg",
   },
 ];
-
--- src/app/components/TracingBeamDemo.tsx
+++ src/app/components/TracingBeamDemo.tsx
@@ -1,4 +1,3 @@
-
 // "use client";
 // import React from "react";
 // import Image from "next/image";
@@ -69,7 +68,7 @@
 // this problem by informing surrounding vehicles of upcoming
 // road blockage due to road accident and thus redirecting traffic to
 // another route which leads to faster first aid and resolution of the
-// situation. 
+// situation.
 //         </p>
 //         <br/>
 //         <p>
@@ -94,7 +93,6 @@
 //   },
 // ];
 
-
 "use client";
 import React from "react";
 import Image from "next/image";
@@ -103,26 +101,24 @@ import { TracingBeam } from "../ui/tracing-beams";
 
 export function TracingBeamDemo() {
   return (
-    <TracingBeam className="px-6">
-      <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+    <TracingBeam className='px-6'>
+      <div className='max-w-2xl mx-auto antialiased pt-4 relative'>
         {dummyContent.map((item, index) => (
-          <div key={`content-${index}`} className="mb-10">
-            <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
+          <div key={`content-${index}`} className='mb-10'>
+            <h2 className='bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4'>
               {item.badge}
             </h2>
 
-            <p className={twMerge("text-xl mb-4")}>
-              {item.title}
-            </p>
+            <p className={twMerge("text-xl mb-4")}>{item.title}</p>
 
-            <div className="text-sm prose prose-sm dark:prose-invert">
+            <div className='text-sm prose prose-sm dark:prose-invert'>
               {item?.image && (
                 <Image
                   src={item.image}
-                  alt="blog thumbnail"
-                  height="1000"
-                  width="1000"
-                  className="rounded-lg mb-10 object-cover"
+                  alt='blog thumbnail'
+                  height='1000'
+                  width='1000'
+                  className='rounded-lg mb-10 object-cover'
                 />
               )}
               {item.description}
@@ -140,32 +136,52 @@ const dummyContent = [
     description: (
       <>
         <p>
-          Two died and over a dozen commuters were reportedly injured as nearly 40 vehicles met with an accident due to low visibility caused by dense fog in Uttar Pradesh's Bulandshahr.
+          Two died and over a dozen commuters were reportedly injured as nearly
+          40 vehicles met with an accident due to low visibility caused by dense
+          fog in Uttar Pradesh Bulandshahr.
         </p>
       </>
     ),
     badge: "",
-    image: "https://i.pinimg.com/736x/7e/7f/6f/7e7f6f714bbef33b5aa0d275c410d093.jpg",
+    image:
+      "https://i.pinimg.com/736x/7e/7f/6f/7e7f6f714bbef33b5aa0d275c410d093.jpg",
   },
   {
     title: "Abstract",
     description: (
       <>
         <p>
-          Main focus of our system is to prevent multiple accidents occurring due to low light conditions or low visibility due to fog and smog conditions. Many times in such conditions, multiple accident occur due to delay in transfer of information of accident ahead on road which leads to traffic jam or car smashing into debris of previous accident. Our system resolves this problem by informing surrounding vehicles of upcoming road blockage due to road accident and thus redirecting traffic to another route which leads to faster first aid and resolution of the situation.
+          Main focus of our system is to prevent multiple accidents occurring
+          due to low light conditions or low visibility due to fog and smog
+          conditions. Many times in such conditions, multiple accident occur due
+          to delay in transfer of information of accident ahead on road which
+          leads to traffic jam or car smashing into debris of previous accident.
+          Our system resolves this problem by informing surrounding vehicles of
+          upcoming road blockage due to road accident and thus redirecting
+          traffic to another route which leads to faster first aid and
+          resolution of the situation.
         </p>
         <br />
         <p>
-          Secondary objective of our system is to reduce the delay in emergency services. Our system directly contacts emergency services in case of accident so required assistance and medical help can be sent in minimal time increasing the mortality rate.
+          Secondary objective of our system is to reduce the delay in emergency
+          services. Our system directly contacts emergency services in case of
+          accident so required assistance and medical help can be sent in
+          minimal time increasing the mortality rate.
         </p>
         <br />
         <p>
-          Overall, our systems aims to reduce damage done to life and property due to delay in information relay. This technology is centralized and must need concept in today's context as more and more people choose private transport which results in increasing number of vehicle on road day by day. This further results into ever growing rate of accident and irreversible damage to economy which this unfortunate incidents bring
+          Overall, our systems aims to reduce damage done to life and property
+          due to delay in information relay. This technology is centralized and
+          must need concept in today context as more and more people choose
+          private transport which results in increasing number of vehicle on
+          road day by day. This further results into ever growing rate of
+          accident and irreversible damage to economy which this unfortunate
+          incidents bring
         </p>
       </>
     ),
     badge: "",
-    image: "https://www.cartoq.com/wp-content/uploads/2017/11/Fog-Crash-on-the-Agra-Lucknow-Expressway.jpg",
+    image:
+      "https://www.cartoq.com/wp-content/uploads/2017/11/Fog-Crash-on-the-Agra-Lucknow-Expressway.jpg",
   },
 ];
-
